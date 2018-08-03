1fc39e5b60d8f9b72e6f6b4dba637b10d4995813
alanliu <alanliu@cybersoft4u.com>
2018-08-03 17:23:14 +0800

    init

---
+++ FlowAgent.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+import logging
+
+
+class FlowAgent(object):
+
+    def __init__(self, profile):
+        self._profile = profile
+
+    def run_daka(self):
+        pass
