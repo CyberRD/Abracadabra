a17c4118936d79fd5ea0a0101b8ee4d29bfaf018
Edward Wu <edwardwu@cybersoft4u.com>
2018-08-06 17:13:28 +0800

    fix

--- Device/Andriod.py
+++ Device/Andriod.py
@@ -2,6 +2,7 @@
 import logging
 from appium import webdriver
 from datetime import datetime
+from appium.webdriver.common.touch_action import TouchAction
 
 class AndriodDevice(object):
 
@@ -9,13 +10,15 @@ class AndriodDevice(object):
         self.desired_caps = {}
         self.desired_caps['platformName'] = 'Android'
         self.desired_caps['platformVersion'] = '7.0'
-        # desired_caps['platformVersion'] = '8.0'
+        #self.desired_caps['platformVersion'] = '8.0'
         self.desired_caps['deviceName'] = 'WUJ01N47TY'
-        # desired_caps['deviceName'] = 'CB512BKD88'
+        #self.desired_caps['deviceName'] = 'CB512BKD88'
         self.desired_caps['appPackage'] = 'com.cybersoft.had'
         self.desired_caps['appActivity'] = 'com.cybersoft.had.activity.MainActivity'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
         self.driver.implicitly_wait(10)
+
+        self.first_day_index = None
 
     def launch_app(self):
         print('launch_app')
@@ -39,8 +42,12 @@ class AndriodDevice(object):
 
     def handle_non_workday_alert(self):
         print('handle_non_workday_alert')
-        ok = self.driver.find_element_by_id('com.cybersoft.had:id/md_buttonDefaultPositive')
-        ok.click()
+        try:
+            self.driver.find_element_by_id('com.cybersoft.had:id/md_buttonDefaultPositive')
+            ok.click()
+        except:
+            pass
+        #TouchAction(self.driver).tap(x=1, y=1).perform()
 
 
     def punch(self, hour, minute):
@@ -68,6 +75,7 @@ class AndriodDevice(object):
         print('switch_calendar_to_last_month')
         pre_page = self.driver.find_element_by_id('com.cybersoft.had:id/ibPrev')
         pre_page.click()
+        self.first_day_index = None
 
 
     def back_page(self):
@@ -75,21 +83,22 @@ class AndriodDevice(object):
         self.driver.keyevent(4)
 
 
-    def find_xpath_of_day(self, day):
+    def _find_xpath_of_day(self, day):
+
         def get_date_xpath(calender_index):
             return \
                 '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.GridView/android.widget.LinearLayout[{calender_index}]/android.widget.LinearLayout/android.widget.TextView'.format(
                     calender_index=calender_index)
-
-        calender_index = 0
-        while True:
-            calender_index += 1
-            date_xpath = get_date_xpath(calender_index)
-            first_day = self.driver.find_element_by_xpath(date_xpath)
-            if first_day.text == '1':
-                first_day_index = calender_index
-                break
-        target_day_index = first_day_index + day - 1
+        if not self.first_day_index:
+            calender_index = 0
+            while True:
+                calender_index += 1
+                date_xpath = get_date_xpath(calender_index)
+                first_day = self.driver.find_element_by_xpath(date_xpath)
+                if first_day.text == '1':
+                    self.first_day_index = calender_index
+                    break
+        target_day_index = self.first_day_index + day - 1
         return get_date_xpath(target_day_index)
 
     def click_on_work(self):
@@ -101,14 +110,20 @@ class AndriodDevice(object):
         sign_in = self.driver.find_element_by_id('com.cybersoft.had:id/btnOff')
         sign_in.click()
 
-    def switch_calendar_to_end_month(self, target_month):
+    def switch_calendar_to_end_month(self, target_date):
         #month = driver.find_element_by_id('com.cybersoft.had:id/tvMonth')
         #month_year = datetime.strptime(month.text, "%B, %Y")
-        current_month = datetime.now().month
-        month_diff = current_month - target_month
+        current_date = datetime.now()
+        month_diff = current_date.month - target_date.month
         last_page = self.driver.find_element_by_id('com.cybersoft.had:id/ibPrev')
         for i in range(month_diff):
             last_page.click()
+        self.first_day_index = None
+
+    def choose_date_from_calendar(self, day):
+        day_xpath = self._find_xpath_of_day(day)
+        self.driver.find_element_by_xpath(day_xpath).click()
+
 
 
 
